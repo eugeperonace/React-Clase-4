dcd67c7d1fd7f91b8426e73075707d84bb2153b2
Eugenia Peronace <eugeperonace@gmail.com>
2022-07-02 21:28:41 -0300

    Prueba desafío 5 - Promesas

--- src/Components/ItemListContainer/index.js
+++ src/Components/ItemListContainer/index.js
@@ -1,18 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from 'react'
 import "./styles.css";
 import ItemCount from "../ItemCount/itemCount";
+import ItemList from '../ItemList/ItemList'
+import { getData } from '../../mocks/fakeApi'
 
 const ItemListContainer = ({greeting}) => {
+
+    const [productList, setProductList]=useState([])
+    const [loading, setLoading]=useState(true)
+
+    //tratar la promesa con then catch finally
+        useEffect(()=>{
+        getData
+        .then((result)=> setProductList(result))
+        .catch((error)=> console.log(error))
+        .finally(()=>setLoading(false) )
+        },[])
 
     const onAdd = (quantity) =>{
         console.log(`¡Compraste ${quantity} unidades!`);
     }
 
     return(
+        <>
         <div className="landing">
             <span>{greeting}</span>
             <ItemCount initial={1} stock={5} onAdd={onAdd}/>
         </div>
+        <div>
+        <h1>{greeting}</h1>
+        {loading ? <p>Cargando...</p> : <ItemList productList={productList}/> }
+        
+      </div>
+        </>
     );
 };
 
